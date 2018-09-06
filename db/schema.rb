9998de51892d6c2c95008d8a099f0f55161f1e18
devanshu-m <devanshu_m@gwmail.gwu.edu>
2018-09-06 15:40:06 -0400

    Adds DataFeed model
    Creates DataFeed model with columns corresponding to registrar's feed.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,43 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180906144113) do
+ActiveRecord::Schema.define(version: 20180906181146) do
+
+  create_table "data_feeds", force: :cascade do |t|
+    t.string "etd_record_key"
+    t.string "public_person_ID"
+    t.string "directory_first_name"
+    t.string "directory_middle_name"
+    t.string "directory_last_name"
+    t.string "email"
+    t.string "home_address_1"
+    t.string "home_address_2"
+    t.string "home_address_3"
+    t.string "home_address_city"
+    t.string "home_address_state"
+    t.string "home_address_postal_code"
+    t.string "home_address_country_code"
+    t.string "home_address_country_descr"
+    t.string "ferpa_suppression_flag"
+    t.string "acad_career_code"
+    t.string "acad_career_descr"
+    t.string "acad_program_code"
+    t.string "acad_program_descr"
+    t.string "primary_acad_plan_code"
+    t.string "primary_acad_plan_descr"
+    t.string "primary_acad_plan_dept_code"
+    t.string "primary_acad_plan_dept_descr"
+    t.string "secondary_acad_plan_code"
+    t.string "secondary_acad_plan_descr"
+    t.string "secondary_acad_plan_dept_code"
+    t.string "secondary_acad_plan_dept_descr"
+    t.string "program_status_descr"
+    t.string "degree_code"
+    t.string "degree_status_descr"
+    t.date "degree_status_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false

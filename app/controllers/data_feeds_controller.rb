e4c06eaf5f4438e4fe5eefe42a660fc7fedded33
Alex Zotov <alexandr.zotov@emory.edu>
2018-10-17 10:53:46 -0400

    filter ip address

--- app/controllers/data_feeds_controller.rb
+++ app/controllers/data_feeds_controller.rb
@@ -1,4 +1,14 @@
 class DataFeedsController < ApplicationController
+  before_action :filter_ip_address
+  
   def home
   end
+
+
+  private
+    
+    def filter_ip_address
+    	requestor = Integration.where(ip: request.remote_ip).present?
+    	head :unauthorized unless requestor
+    end
 end
